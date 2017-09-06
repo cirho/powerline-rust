d01d063aca6f5b365b89730ecc92f7012bd71e52
Xeoeen <Xeoeen@users.noreply.github.com>
2017-09-06 16:29:03 +0200

    Simplifiled code in cwd segment

--- src/segments/cwd.rs
+++ src/segments/cwd.rs
@@ -10,29 +10,23 @@ fn pb_to_str(path: path::PathBuf) -> String {
 pub fn add_segment(prompt : &mut Powerline, special: &str) {
     let cwd = pb_to_str(env::current_dir().unwrap());
     let mut cwd_slice = cwd.get(0..).unwrap();
-    match env::home_dir() {
-        Some(home_path) => {
-            let home = pb_to_str(home_path);
-            match cwd.find(&home) {
-                    Some(pos) => {
-                    cwd_slice = cwd.get((pos + home.len() )..).unwrap();
-                    prompt.add_segment(Segment::simple(&format!(" {} ", special), Color::HOME_FG, Color::HOME_BG) )
-                    },
-                None => {},
-            }
-        },
-        None => {},
+    if let Some(home_path) = env::home_dir() {
+        let home = pb_to_str(home_path);
+        if let Some(pos) = cwd.find(&home) {
+            cwd_slice = cwd.get((pos + home.len() )..).unwrap();
+            prompt.add_segment(Segment::simple(&format!(" {} ", special), Color::HOME_FG, Color::HOME_BG) )
+        }
     }
-    let path: Vec<&str> = cwd_slice.split("/").skip(1).collect();
-    let size = path.len();
-    for idx in 0..size {
-        let mut el = &path[idx];
-        if el == &"" { el = &"/" }
-        if idx != size - 1 {
-            prompt.add_segment(Segment::special(&format!(" {} ", el), Color::PATH_FG, Color::PATH_BG, '\u{E0B1}', Color::SEPARATOR_FG ) );
-        }
-        else {
-            prompt.add_segment(Segment::simple(&format!(" {} ", el), Color::PATH_FG, Color::PATH_BG) );
-        }
+
+    let mut counter = 0;
+    for val in cwd_slice.split("/").skip(1) {
+        prompt.add_segment(Segment::special(&format!(" {} ", val), Color::PATH_FG, Color::PATH_BG, '\u{E0B1}', Color::SEPARATOR_FG ) );
+        counter += 1
+    }
+    if counter > 0 {
+        let last = prompt.last_segment_mut().unwrap();
+        if last.val == "  " { last.val = " / ".to_owned()}
+        last.sep = '\u{E0B0}';
+        last.sep_col = last.bg;
     }
 }
