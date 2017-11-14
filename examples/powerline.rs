3d52166a643abfb796a237283831d03302e6b132
cirho <me@cirho.com>
2017-11-14 18:10:03 +0100

    Theme support based on matcegla pull request

--- examples/powerline.rs
+++ examples/powerline.rs
@@ -4,7 +4,8 @@ use powerline::segments::*;
 use powerline::part::*;
 
 fn main() {
-    let mut prompt = powerline::Powerline::new();
+
+    let mut prompt = powerline::Powerline::new(powerline::theme::DEFAULT_THEME);
     prompt.add_segments(user::User::new().get_segments().expect("Failed seg: User"));
     prompt.add_segments(host::Host::new().get_segments().expect("Failed seg: Host"));
     prompt.add_segments(cwd::Cwd::new("~").get_segments().expect("Failed seg: Cwd"));
