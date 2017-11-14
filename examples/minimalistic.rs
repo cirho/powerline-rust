eae198b2884a20d4fcdf268a2fc1411a5fc3c67b
Xeoeen <Xeoeen@users.noreply.github.com>
2017-11-14 19:20:22 +0100

    Dirty cwd shorting

--- examples/minimalistic.rs
+++ examples/minimalistic.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let mut prompt = powerline::Powerline::new(powerline::theme::DEFAULT_THEME);
 
-    prompt.add_segments(cwd::Cwd::new("~").get_segments().expect("Failed seg: Cwd"));
+    prompt.add_segments(cwd::Cwd::new("~", 45, 4).get_segments().expect("Failed seg: Cwd"));
     prompt.add_segments(git::GitInfo::new().get_segments().expect("Failed seg: Git"));
     prompt.add_segments(readonly::ReadOnly::new("").get_segments().expect("Failed seg: ReadOnly"));
     prompt.add_segments(cmd::Cmd::new("$", "#").get_segments().expect("Failed seg: Cmd"));
