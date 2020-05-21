c470a38ff966e5313c03695949fd270deeab6659
cirho <me@cirho.com>
2020-05-21 17:24:15 +0200

    remove useless alloc

--- src/modules/exit_code.rs
+++ src/modules/exit_code.rs
@@ -20,7 +20,8 @@ impl<S: ExitCodeScheme> ExitCode<S> {
 
 impl<S: ExitCodeScheme> Module for ExitCode<S> {
 	fn append_segments(&mut self, segments: &mut Vec<Segment>) -> R<()> {
-		let exit_code = env::args().nth(1).unwrap_or("1".to_string());
+		let exit_code = env::args().nth(1).unwrap_or_else(|| "1".to_string());
+
 		if exit_code != "0" {
 			let (fg, bg) = (S::EXIT_CODE_FG, S::EXIT_CODE_BG);
 			segments.push(Segment::simple(format!(" {} ", exit_code), fg, bg));
