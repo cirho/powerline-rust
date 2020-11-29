b257da5e2f644e60d6c832cda345a62db8e7febe
Andrew Rowson <github@growse.com>
2020-11-29 11:27:21 +0000

    Use std::error::Error

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,8 @@
-mod error;
 pub mod modules;
 pub mod powerline;
 pub mod terminal;
 pub mod theme;
 pub(crate) mod utils;
 
-pub type R<T> = Result<T, error::Error>;
+pub type R<T> = Result<T, Box<dyn std::error::Error>>;
 pub use crate::powerline::{Powerline, Segment};
-pub use error::Error;
